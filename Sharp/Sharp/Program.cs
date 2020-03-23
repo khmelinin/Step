83019a733b9e777ebef8a7170eea55e6aa7a41e5
Hard <khmelinin2001@gmail.com>
2020-03-23 19:42:38 +0200

    adding sharp solution

---
+++ Sharp/Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
