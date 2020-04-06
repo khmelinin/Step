f3e4241ec4c9b2033889c19ed1b99372eed64340
Hard <khmelinin2001@gmail.com>
2020-04-06 22:01:14 +0300

    9

---
+++ Sharp/9(operators_overload)/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace _9_operators_overload_
+{
+    public class Point
+    {
+        private int x, y;
+        public Point(int xPos, int yPos)
+        {
+            x = xPos;
+            y = yPos;
+        }
+        public override string ToString()
+        {
+            return string.Format("[{0}, {1}]",this.x,this.y);
+        }
+
+        public static Point operator +(Point a, Point b)
+        {
+            return new Point(a.x + b.x, a.y + b.y);
+        }
+        public static Point operator -(Point a, Point b)
+        {
+            return new Point(a.x - b.x, a.y - b.y);
+        }
+        public static Point operator *(Point a, Point b)
+        {
+            return new Point(a.x * b.x, a.y * b.y);
+        }
+        public static Point operator /(Point a, Point b)
+        {
+            return new Point(a.x / b.x, a.y / b.y);
+        }
+
+        public static Point operator ++(Point a)
+        {
+            return new Point(a.x +1, a.y +1);
+        }
+        public static Point operator --(Point a)
+        {
+            return new Point(a.x - 1, a.y - 1);
+        }
+        public static bool operator ==(Point a,Point b)
+        {
+            return a.Equals(b);
+        }
+        public static bool operator !=(Point a, Point b)
+        {
+            return !a.Equals(b);
+        }
+        public int CompareTo(object obj)
+        {
+            if(obj is Point)
+            {
+                Point p = (Point)obj;
+                if (this.x > p.x && this.y > p.y)
+                    return 1;
+
+                else if (this.x < p.x && this.y < p.y)
+                    return -1;
+                else 
+                    return 0;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+        public static bool operator <(Point a, Point b)
+        {
+            return (a.CompareTo(b)<0);
+        }
+        public static bool operator >(Point a, Point b)
+        {
+            return (a.CompareTo(b) > 0);
+        }
+
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(1, 3);
+            Point b = new Point(6, 7);
+            a++;
+            Point c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ Sharp/9/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace _9
+{
+    class Point : object
+    {
+        protected int x, y;
+        public Point(int xx, int yy)
+        {
+            x = xx;
+            y = yy;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+                return false;
+            Point p = (Point)obj;
+
+            return (x == p.x) && (y == p.y);
+        }
+        public override int GetHashCode()
+        {
+            return x - y;
+        }
+    }
+
+    class MyBaseClass
+    {
+        public static string CompanyName = "MS";
+        public int age;
+        public string name;
+    }
+
+    class A { public int a = 1; }
+    class B { public int b = 2; }
+    class C { public B B = new B(); }
+    class X
+    {
+        public A A = new A();
+        public C C = new C();
+    }
+
+
+    //class MyDerivedClass:MyBaseClass
+    class Program : X
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Object a = new Point(-10, -1);
+            Object b = new Point(-10, -1);
+            Object c = new Point(10, 0);
+
+            Console.WriteLine("a == b : {0}",a.Equals(b));
+            Console.WriteLine("HashA : {0}",a.GetHashCode());
+            Console.WriteLine("HashB : {0}",b.GetHashCode());
+            Console.WriteLine("HashC : {0}",c.GetHashCode());
+            */
+            /*
+            MyDerivedClass original = new MyDerivedClass();
+            original.age = 24;
+            original.name = "Alyx";
+            MyDerivedClass.CompanyName = "Black Mesa";
+            Console.WriteLine(original.age + " " + original.name + " " + MyDerivedClass.CompanyName);
+            MyDerivedClass clone = original.MemberwiseClone() as MyDerivedClass;
+            clone.age = 43;
+            clone.name = "Gordon";
+            Console.WriteLine(clone.age + " " + clone.name + " " + MyDerivedClass.CompanyName);
+            */
+            /*
+            Program original = new Program();
+            Console.WriteLine("original : "+original.A.a+" "+original.C.B.b);
+            Program clone = original.MemberwiseClone() as Program;
+            Console.WriteLine("clone : " + clone.A.a + " " + clone.C.B.b);
+            clone.A.a = clone.C.B.b = 7;
+            Console.WriteLine("original : " + original.A.a + " " + original.C.B.b);
+            Console.WriteLine("clone : " + clone.A.a + " " + clone.C.B.b);
+            original.A.a = original.C.B.b = 3;
+            Console.WriteLine("original : " + original.A.a + " " + original.C.B.b);
+            Console.WriteLine("clone : " + clone.A.a + " " + clone.C.B.b);
+            */
+
+
+        }
+    }
+}
+
