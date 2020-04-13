6b90332e61741a7be39e7b2907aa400913233de7
Hard <khmelinin2001@gmail.com>
2020-04-13 21:57:37 +0300

    13

---
+++ Sharp/13(lambda)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _13_lambda_
+{
+    class Program
+    {
+        public delegate double MyDelegate(double a, double b);
+        static void Main(string[] args)
+        {
+            string z = "+";
+            double num1 = 1, num2 = 3;
+            MyDelegate myDelegate= null;
+            z = Console.ReadLine();
+            if (z == "+")
+                myDelegate = (a, b) => a + b;
+            else
+                if (z == "-")
+                myDelegate = (a, b) => a - b;
+            else
+                if (z == "*")
+                myDelegate = (a, b) => a * b;
+            else
+                if (z == "/")
+                myDelegate = (a, b) => { if (b != 0) return a / b; else { Console.WriteLine("(num/0) no no no"); return 0; } };
+            else
+                myDelegate = (a, b) => { Console.WriteLine("wrong operator");return 0; };
+            
+            double sum = myDelegate(num1, num2);
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
---
+++ Sharp/13/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace _13
+{
+    delegate void Listener();
+    class Button
+    {
+        public Listener buttonListener;
+        public void OnClick()
+        {
+            buttonListener();
+        }
+    }
+    class Car
+    {
+         
+        public void Move()
+        {
+            Console.WriteLine("Move from Car");
+        }
+    }
+
+    class Road
+    {
+        
+        public void State()
+        {
+            Console.WriteLine("Move from Road");
+        }
+    }
+    class Program
+    {
+        public delegate bool Logika_1(int x);
+        static void Check(Logika_1 log, int a)
+        {
+            if(log(a))
+            {
+                Console.WriteLine("> 0");
+            }
+            else
+            {
+                Console.WriteLine("< 0");
+            }
+        }
+        static bool LOG_1(int d)
+        {
+            Console.WriteLine("LOG_1");
+            return d > 0;
+        }
+
+        static bool LOG_2(int t)
+        {
+            Console.WriteLine("LOG_2");
+            return (t-1) > 0;
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+            Car car = new Car();
+            Road road = new Road();
+            Button button = new Button();
+            button.buttonListener = new Listener(road.State);
+            button.buttonListener += car.Move;
+            button.OnClick();
+            Console.WriteLine("____________________");
+            button.buttonListener -= car.Move;
+            button.OnClick();
+            */
+
+            Logika_1 logike = new Logika_1(LOG_1);
+            logike(68);
+            Check(logike,6);
+            Console.WriteLine("______________");
+            logike += LOG_2;
+            logike -= LOG_1;
+            Check(logike, -6);
+
+        }
+    }
+}
