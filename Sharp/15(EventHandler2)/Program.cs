1ee2e921e92089b9139eb4ba1d3c22704e87600a
Hard <khmelinin2001@gmail.com>
2020-04-15 22:01:46 +0300

    15

---
+++ Sharp/15(EventHandler2)/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace _15_EventHandler2
+{
+    class ContainerInfo : EventArgs
+    {
+        public string Name;
+    }
+    class EventClass
+    {
+        public event EventHandler<ContainerInfo> EventTest_1;
+        public event EventHandler<ContainerInfo> ColorA;
+        public event EventHandler<ContainerInfo> ColorB;
+        public void Letter(string nm)
+        {
+            ContainerInfo cInfo = new ContainerInfo();
+            cInfo.Name = nm;
+            EventTest_1?.Invoke(this, cInfo);
+        }
+        public void Color_A(string nm)
+        {
+            ContainerInfo cInfo = new ContainerInfo();
+            cInfo.Name = nm;
+            ColorA?.Invoke(this, cInfo);
+        }
+        public void Color_B(string nm)
+        {
+            ContainerInfo cInfo = new ContainerInfo();
+            cInfo.Name = nm;
+            ColorB?.Invoke(this, cInfo);
+        }
+    }
+
+    class Program
+    {
+        static void HandlerA(object sen, ContainerInfo ee)
+        {
+            if (ee.Name == "a")
+            {
+                Console.WriteLine("i am 'a' ");
+                Console.WriteLine("enter color = ");
+                string color = Console.ReadLine();
+                EventClass evClass = (EventClass)sen;
+                evClass.Color_A(color);
+            }
+            else
+                Console.WriteLine("a: no no no");
+        }
+        static void HandlerB(object sen, ContainerInfo ee)
+        {
+            if (ee.Name == "b")
+            {
+                Console.WriteLine("i am 'b' ");
+                Console.WriteLine("enter color = ");
+                string color = Console.ReadLine();
+                EventClass evClass = (EventClass)sen;
+                evClass.Color_B(color);
+            }
+            else
+                Console.WriteLine("b: no no no");
+        }
+
+        static void HandlerA_Yellow(object sen, ContainerInfo ee)
+        {
+            if (ee.Name == "yellow")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("a");
+                Console.ResetColor();
+            }
+            else
+                Console.WriteLine("a_yellow: no no no");
+        }
+        static void HandlerA_Red(object sen, ContainerInfo ee)
+        {
+            if (ee.Name == "red")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("a");
+                Console.ResetColor();
+            }
+            else
+                Console.WriteLine("a_red: no no no");
+
+        }
+
+        static void HandlerB_Green(object sen, ContainerInfo ee)
+        {
+            if (ee.Name == "green")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("b");
+                Console.ResetColor();
+            }
+            else
+                Console.WriteLine("b_green: no no no");
+        }
+
+        static void HandlerB_Blue(object sen, ContainerInfo ee)
+        {
+            if (ee.Name == "blue")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("b");
+                Console.ResetColor();
+            }
+            else
+                Console.WriteLine("b_blue: no no no");
+        }
+        static void Main(string[] args)
+        {
+            EventClass mc = new EventClass();
+            mc.EventTest_1 += HandlerA;
+            mc.EventTest_1 += HandlerB;
+            mc.ColorA += HandlerA_Yellow;
+            mc.ColorA += HandlerA_Red;
+            mc.ColorB += HandlerB_Blue;
+            mc.ColorB += HandlerB_Green;
+
+            while (true)
+            {
+                Console.WriteLine("Enter letter: ");
+                string s = Console.ReadLine();
+                mc.Letter(s);
+            }
+        }
+    }
+}
---
+++ Sharp/15/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _15
+{
+    class ContainerInfo:EventArgs
+    {
+        public int number;
+        public string Name;
+    }
+    class MyClass
+    {
+        public event EventHandler<ContainerInfo> EventTest_1;
+        public void StartEvent(int n, string nm)
+        {
+            ContainerInfo cInfo = new ContainerInfo();
+            cInfo.number = n;
+            cInfo.Name = nm;
+            EventTest_1?.Invoke(this, cInfo);
+        }
+    }
+    class Program
+    {
+        static void Handler1(object sen, ContainerInfo ee)
+        {
+            Console.WriteLine("Handler1" + sen.GetType());
+            Console.WriteLine(ee.number);
+            Console.WriteLine(ee.Name);
+        }
+        static void Handler2(object sen, ContainerInfo ee)
+        {
+            Console.WriteLine("Handler2"+sen.GetType());
+            Console.WriteLine(ee.number);
+            Console.WriteLine(ee.Name);
+        }
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            mc.EventTest_1 += Handler1;
+            mc.StartEvent(3,"chuvk");
+            mc.EventTest_1 += Handler2;
+            mc.StartEvent(4, "4chuvk4");
+        }
+    }
+}
