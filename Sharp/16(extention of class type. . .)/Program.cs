73919ec094ad0ecff777133c9d59001f1142be81
Hard <khmelinin2001@gmail.com>
2020-04-16 20:21:35 +0300

    16(tmp)

---
+++ Sharp/16(extention of class type. . .)/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _16_extention_of_class_type._._._
+{
+    class MyClass<T1, T2>
+    {
+        
+
+        public T1 field1;
+        public T2 field2;
+        public U Method<T3, U>(T3 arg, U v)
+        {
+            Console.WriteLine("field1 = " + field1.GetType());
+            Console.WriteLine("field2 = " + field2.GetType());
+            Console.Write("return = ");
+            Console.WriteLine(arg);
+            U val = v;
+            return val;
+        }
+    }
+
+    class MyClass1<TT>
+    {
+        TT field;
+    }
+
+    delegate R MyDelegate<Y, R>(Y arg);
+
+    class Program
+    {
+        static int Add(double d)
+        {
+            return Convert.ToInt32(d);
+        }
+        static string Concantenation(string s)
+        {
+            return "Hello " + s;
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            MyClass<int,int> a = new MyClass<int,int>();
+            Console.WriteLine(a.Method<int, bool>(3, true));
+            MyClass<string,int> b = new MyClass<string,int>();
+            b.field1 = "adf";
+            Console.WriteLine(b.Method<string,double>("str",7.62));
+
+            MyDelegate<double, int> delegate1 = new MyDelegate<double, int>(Add);
+            int aa = delegate1(12.6);
+            MyDelegate<string, string> delegate2 = new MyDelegate<string, string>(Concantenation);
+            string st = delegate2("Zorro");
+        }
+    }
+}
---
+++ Sharp/16/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _16
+{
+    class Program
+    {
+        public delegate string Message();
+        public delegate void ArrMessage(Message[] m);
+        public static string Method_1()
+        {
+            return "Method_1";
+        }
+        public static string Method_2()
+        {
+            return "Method_2";
+        }
+        public static string Method_3()
+        {
+            return "Method_3";
+        }
+        public static string Method_4()
+        {
+            return "Method_4";
+        }
+        public static string Method_5()
+        {
+            return "Method_5";
+        }
+        static void Main(string[] args)
+        {
+            Message[] arr = new Message[5];
+            arr[0] = new Message(Method_1);
+            arr[1] = new Message(Method_2);
+            arr[2] = new Message(Method_3);
+            arr[3] = new Message(Method_4);
+            arr[4] = new Message(Method_5);
+
+            ArrMessage arrm = delegate (Message[] arr)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                    Console.WriteLine(arr[i].Invoke());
+
+            };
+
+            arrm.Invoke(arr);
+        }
+
+
+
+    }
+}
