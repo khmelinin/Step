0108c5459ea210291e001fdb4e21459c05a6492d
Hard <khmelinin2001@gmail.com>
2020-05-25 22:16:06 +0300

    9

---
+++ WindowsForms/9(Proggres bar)/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace _9_Proggres_bar_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 121);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(335, 39);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Press to start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Value = 0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Reset progress";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 30);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(138, 30);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 5;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(15, 82);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Start";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Stop";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Step";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(12, 261);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(335, 39);
+            this.progressBar2.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar2.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 306);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 38);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Press to start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 235);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(297, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 235);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Path:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(379, 29);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(271, 379);
+            this.textBox2.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 429);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsForms/9(Proggres bar)/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9_Proggres_bar_
+{
+    public partial class Form1 : Form
+    {
+        private VScrollBar vScroll;
+        private HScrollBar hScroll;
+        private List<string> tmp = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            button2.Enabled = false;
+
+            vScroll = new VScrollBar();
+            vScroll.Dock = DockStyle.Right;
+            vScroll.Scroll += vs_scroll;
+            this.Controls.Add(vScroll);
+            
+            hScroll = new HScrollBar();
+            hScroll.Dock = DockStyle.Bottom;
+            hScroll.Scroll += vs_scroll;
+            this.Controls.Add(hScroll);
+
+            
+        }
+
+        
+
+        void LoadFromTxt(string p)
+        {
+            var path = "1.txt";
+            
+            if (p.Length > 0)
+            {
+                path = p;
+            }
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line;
+
+                    for (int i = 0; (line = sr.ReadLine()) != null; i++)
+                    {
+                        tmp.Add(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void Read(string p)
+        {
+            LoadFromTxt(p);
+            progressBar2.Maximum = tmp.Count-1;
+            progressBar2.PerformStep();
+            for (int i = progressBar2.Minimum; i <= progressBar2.Maximum; i += progressBar2.Step)
+            {
+                textBox2.Text += tmp[i];
+                textBox2.Text += "\n"; ////////////////////////////////////////////////////////////////////// <- все равно не ставит Enter
+                progressBar2.PerformStep();
+                this.Update();
+                Thread.Sleep(100);
+            }
+            
+        }
+
+        private void vs_scroll(object sender, EventArgs e)
+        {
+            this.Text = $"VS: {vScroll.Value}; HS: {hScroll.Value}";
+        }
+
+        private bool Validation()
+        {
+            if (numericUpDown1.Value < numericUpDown2.Value && numericUpDown3.Value < numericUpDown2.Value - numericUpDown1.Value && numericUpDown3.Value>numericUpDown1.Value)
+            {
+                progressBar1.Minimum = Convert.ToInt32(numericUpDown1.Value);
+                progressBar1.Maximum = Convert.ToInt32(numericUpDown2.Value);
+                progressBar1.Step = Convert.ToInt32(numericUpDown3.Value);
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("ProgressBar min,max,step incorrect value", "Value Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            if (Validation())
+            {
+                progressBar1.PerformStep();
+                for (int i = progressBar1.Minimum; i <= progressBar1.Maximum; i += progressBar1.Step)
+                {
+                    progressBar1.PerformStep();
+
+                    label1.Text = $@"Value = {progressBar1.Value}";
+                    this.Update();
+                    Thread.Sleep(progressBar1.Step * 1000);
+                }
+                button2.Enabled = true;
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = progressBar1.Minimum;
+            label1.Text = "Value = 0";
+            this.Update();
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            progressBar2.Value = 0;
+            progressBar2.Minimum = 0;
+            progressBar2.Step = 1;
+            Read(textBox1.Text);
+            button3.Enabled = true;
+        }
+    }
+}
