c7825c331a84a90920a9688192a91f245e5e54a2
Hard <khmelinin2001@gmail.com>
2020-06-25 22:05:55 +0300

    8

---
+++ WPF/8(trigger)/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _8_trigger_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Style Style1obj = new Style();
+            //button2.Style = Style1obj;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
