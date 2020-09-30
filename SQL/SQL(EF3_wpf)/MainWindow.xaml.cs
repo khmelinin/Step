e550bdb5dff1af450a45e161b802a884abfc98ce
Hard <khmelinin2001@gmail.com>
2020-09-30 20:47:18 +0300

    sql_ef3

---
+++ SQL/SQL(EF3_wpf)/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQL_EF3_wpf_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Student> _students;
+        private List<Group> _groups;
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            using (var db = new schoolEntities())
+            {
+                _students = db.Students.Local.ToList();
+                _groups = db.Groups.Local.ToList();
+            }
+            gridStudents.ItemsSource = _students;
+            gridGroups.ItemsSource = _groups;
+        }
+    }
+}
