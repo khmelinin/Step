b457887e4205a6b9ef5c289cd6d7ad2d350f3174
AdmiN <khmelinin2001@gmail.com>
2020-10-12 22:16:35 +0300

    Processors

---
+++ Processors/Processors/P1/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace P1.ViewModels
+{
+    class MainViewModel:BindableBase
+    {
+        private ObservableCollection<ProcessViewModel> processes;
+        private ProcessViewModel currentItem;
+        private ICommand refreshCommand;
+        private ICommand createCommand;
+
+        public MainViewModel()
+        {
+            processes = new ObservableCollection<ProcessViewModel>(from p in Process.GetProcesses() select new ProcessViewModel(p));
+            refreshCommand = new DelegateCommand(() => { });
+            createCommand = new DelegateCommand(() =>
+            {
+                string filename = string.Empty;
+                var dlg = new OpenFileDialog();
+                dlg.Filter = "Applications (.exe)|*.exe|";
+                if (dlg.ShowDialog() == true)
+                {
+                    filename = dlg.FileName;
+                    Process.Start(filename);
+                }
+            });
+        }
+
+        public ObservableCollection<ProcessViewModel> Processes
+        {
+            get => this.processes;
+        }
+
+        public ICommand RefreshCommand
+        {
+            get => this.refreshCommand;
+        }
+
+    }
+}
---
+++ Processors/Processors/P1/ViewModels/ProcessViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace P1.ViewModels
+{
+    class ProcessViewModel:BindableBase
+    {
+        private Process p;
+
+        public ProcessViewModel(Process p)
+        {
+            this.p = p;
+        }
+
+        public string Name
+        {
+            get { return p?.ProcessName; }
+        }
+
+        public int? ProcessId
+        {
+            get { return p?.Id; }
+        }
+
+        public DateTime? StartTime
+        {
+            get {
+                try
+                {
+                    return p?.StartTime;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
+        }
+
+        public int? BasePriority
+        {
+            get => p?.BasePriority;
+        }
+
+    }
+}
