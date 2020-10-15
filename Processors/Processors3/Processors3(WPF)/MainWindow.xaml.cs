6687edcddd627343e531061c55674a418bde3303
AdmiN <khmelinin2001@gmail.com>
2020-10-15 21:26:21 +0300

    Processors3

---
+++ Processors/Processors3/Processors3(WPF)/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Processors3_WPF_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Semaphore semaphore;
+
+        bool typeEvent = false;
+        //AutoResetEvent @event;
+        ManualResetEvent @event;
+        ManualResetEvent @exitEvent;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //@event = new AutoResetEvent(false);
+            //@event = new ManualResetEvent(false);
+            exitEvent = new ManualResetEvent(false);
+            semaphore = new Semaphore(0, 2);
+        }
+
+        private void btnCreateEvent_Click(object sender, RoutedEventArgs e)
+        {
+            //if (typeEvent)
+            //{
+            //    @event = new AutoResetEvent(false);
+            //}
+            //else
+            //{
+            //    @event = new ManualResetEvent(false);
+            //}
+
+            //@event.Reset();
+            exitEvent.Reset();
+        }
+
+        private void btnStartThreads_Click(object sender, RoutedEventArgs e)
+        {
+            var pbs = new ProgressBar[] { First, Second, Third, Fourth, Fifth };
+            var threads = new Thread[5];
+            for (int i = 0; i < 5; i++)
+            {
+                threads[i] = new Thread((p) => {
+                    semaphore.WaitOne();
+                    //@event.WaitOne();
+                    Random rnd = new Random();
+                    var pb = p as ProgressBar;
+                    for (double j = 0; j < 100; j += 1) 
+                    {
+                        if (exitEvent.WaitOne(0))
+                        {
+                            break;
+                        }
+
+                        App.Current.Dispatcher.Invoke(() => { pb.Value = j; });
+                        Thread.Sleep(rnd.Next(7, 90));
+                    }
+
+                    //@event.Set();
+                    semaphore.Release();
+                });
+                threads[i].IsBackground = true;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                threads[i].Start(pbs[i]);
+            }
+            //@event.Set();
+            semaphore.Release(2);
+            
+        }
+
+        private void btnStopThreads_Click(object sender, RoutedEventArgs e)
+        {
+            exitEvent.Set();
+        }
+    }
+}
---
+++ Processors/Processors3/Processors3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Processors3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string mutexName = "Mutex_first";
+            if (Mutex.TryOpenExisting(mutexName, System.Security.AccessControl.MutexRights.Synchronize, out Mutex result))
+            {
+                Console.WriteLine("Mutex is created, exit");
+                result.Close();
+                result.Dispose();
+            }
+            result = new Mutex(true, mutexName);
+            Console.WriteLine("Waiting...");
+            Console.ReadLine();
+
+            EventWaitHandle wh;
+            AutoResetEvent are;
+            ManualResetEvent mre;
+        }
+    }
+}
