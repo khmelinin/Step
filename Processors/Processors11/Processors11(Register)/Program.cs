a705f4da0abb5151be49ba05369ad52e48b714b9
AdmiN <khmelinin2001@gmail.com>
2020-10-28 21:59:40 +0200

    Processors11

---
+++ Processors/Processors11/Processors11(Register)/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processors11_Register_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            RegistryKey key = Registry.CurrentUser;
+            var software = key.OpenSubKey(@"SOFTWARE\LH\RegistryTest",
+                RegistryKeyPermissionCheck.ReadWriteSubTree,
+                System.Security.AccessControl.RegistryRights.FullControl
+                );
+            var vc = software.ValueCount;
+            var names = software.GetValueNames();
+            var value = software.GetValue(names[0], 9, RegistryValueOptions.None);
+            //var lh = software.CreateSubKey("LH");
+            //var soft = lh.CreateSubKey("RegistryTest");
+            //soft.SetValue("Test", 5, RegistryValueKind.DWord);
+            */
+        }
+    }
+}
---
+++ Processors/Processors11/Processors11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processors11
+{
+    class Program
+    {
+        static void Factorial(int n)
+        {
+            long res = 1;
+            for (int i = 1; i <=n; i++)
+            {
+                res *= i;
+            }
+            Console.WriteLine($"{n}! = {res}");
+        }
+        static async void FactorialAsync(int n)
+        {
+            Console.WriteLine("Start of async method");
+
+            //await Task.Run(() => { Factorial(n); });
+            //await Task.Run(() => { Factorial(n + 1); });
+            //await Task.Run(() => { Factorial(n + 2); });
+            //await Task.Run(() => { Factorial(n + 3); });
+
+            var t = Task.Run(() => { Factorial(n); });
+            var t1 = Task.Run(() => { Factorial(n + 1); });
+            var t2 = Task.Run(() => { Factorial(n + 2); });
+            var t3 = Task.Run(() => { Factorial(n + 3); });
+            await Task.WhenAll(t, t1, t2, t3);
+
+            Console.WriteLine("End of async method");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Main method");
+            FactorialAsync(1);
+            Console.WriteLine("End Main method");
+            Console.ReadLine();
+        }
+    }
+}
